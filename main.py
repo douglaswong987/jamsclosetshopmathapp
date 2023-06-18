590ac67bd3480bd4f2b062c51d8a3be6af61a970
Doug <douglaswong987@gmail.com>
2023-06-17 23:39:27 -0700

    created templates folder for .html files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,5 @@ def process():
     return render_template('results.html', totals=totals)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
